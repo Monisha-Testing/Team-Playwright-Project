12d2c5c8d4fcfe3860b85ff59657f37049fbcc64
Monisha <monitesting03@gmail.com>
2026-04-18 10:22:04 +0530

    Formsubmission commit

---
+++ utils/FormTestData.ts
@@ -0,0 +1,37 @@
+export const nameTestData = {
+  empty: '',
+  min: 'A',
+  max: 'A'.repeat(50),
+};
+
+export const emailTestData = {
+  empty: '',
+  invalidFormat: 'test@',
+};
+
+export const contactTestData = {
+  empty: '',
+  lessDigits: '12345',
+  moreDigits: '123456789012334666543',
+};
+
+export const dropdownTestData = {
+  default: '',
+  option1: 'Andorra',
+  option2: 'United Arab Emirates',
+  option3: 'Brazil'
+};
+
+export const ValidData = {
+  name: 'Monisha',
+  email: 'moni@gmail.com',
+  contactno: '98245673862',
+  date: '2027-04-09',
+  select: 'Cuba'
+};
+
+export const FeedbackData = {
+ empty: '',
+ FeedbackPlaceholdertext: 'Write Comment...',
+ FeedbackInput: 'Test',
+};
