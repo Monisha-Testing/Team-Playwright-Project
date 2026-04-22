bc7f7d6f412151283a9ccb2deb32a1ca622d22f5
shanmugapriya022 <109014566+shanmugapriya022@users.noreply.github.com>
2026-04-22 14:24:28 +0530

    Update login test to use new locator for Sign In

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -6,7 +6,7 @@ test('Login Test', async ({ page }) => {
   const login = new LoginPage(page);
   await page.goto('https://qabrains.com/practice-site');
   //await login.login('test@example.com', 'password');
-  await page.getByRole('link', { name: 'Sign In' }).click();
+  await page.locator("//span[@class='after']//*[name()='svg']//*[name()='path' and contains(@d,'M313.941 2')]").click();
   await page.goto('https://qabrains.com/auth/login');
   //await page.waitForTimeout(2000);
   await page.locator('#authForm > form > div:nth-child(4) > input')
